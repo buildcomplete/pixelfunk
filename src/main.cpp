fd81bc411833261f1173db4a257171b339032a75
Christian Gleerup <christian@nessie>
2018-02-12 22:24:15 +0100

    Template fun aaaand... generalized drawing to a AnimatedObject, at some point I should perhaps look into the NeoPixelAnimation

--- src/main.cpp
+++ src/main.cpp
@@ -7,7 +7,10 @@ const uint16_t PixelCount = 50;
 const uint8_t DotDataPin = 2;
 #define colorSaturation 128
 
-NeoPixelBus<NeoBrgFeature, NeoEspBitBangMethodBase<NeoEspBitBangSpeed400Kbps>> strip(PixelCount, DotDataPin);
+// define type for the bus we are using
+using MyBus = NeoPixelBus<NeoBrgFeature, NeoEspBitBangMethodBase<NeoEspBitBangSpeed400Kbps>>;
+
+MyBus strip(PixelCount, DotDataPin);
 
 RgbColor RgbColorF(float r, float g, float b)
 {
@@ -31,26 +34,32 @@ void setup()
     Serial.println();
     Serial.println("Running...");
 }
-
-// structure to 'walk' a color on the strip...
-struct Walker
+class AnimatedObject
 {
 public:
+	virtual void Update(float dt, int lim, MyBus& strip) = 0;
+};
 
+// structure to 'walk' a color on the strip...
+class Walker : public AnimatedObject
+{
 	int _position;
+	int _length;
 	float _speed;
 	float _accumulatedTime;
 	RgbColor _color;
 
-	Walker(int position, float speed, RgbColor color)
+public:
+	Walker(int position, int length, float speed, RgbColor color)
 	{
 		_position = position;
+		_length = length;
 		_speed = speed;
 		_accumulatedTime = 0;
 		_color = color;
 	}
 	
-	void Update(float dt, int lim)
+	void Update(float dt, int lim, MyBus& strip)
 	{
 		_accumulatedTime += dt;
 		int steps = _accumulatedTime / _speed;
@@ -60,6 +69,7 @@ public:
 			_position += steps;
 			_position %= lim;
 		}
+		strip.SetPixelColor(_position, _color);
 	}
 };
 
@@ -73,26 +83,27 @@ void updateTimers()
 }
 
 // create 4 walkers...
-Walker mrBlue(0, 0.166f, RgbColorF(0,0,1));
-Walker mrGreen(1, 0.100f, RgbColorF(0,1,0));
-Walker mrRed(2, 0.200f, RgbColorF(1,0,0));
-Walker mrYellow(3, 0.400f, RgbColorF(1,1,0));
+Walker mrBlue(0, 5, 0.166f, RgbColorF(0,0,1));
+Walker mrGreen(1, 5, 0.100f, RgbColorF(0,1,0));
+Walker mrRed(2, 5, 0.200f, RgbColorF(1,0,0));
+Walker mrYellow(3, 5, 0.400f, RgbColorF(1,1,0));
 
+AnimatedObject* animatedObjects[] 
+{
+	&mrBlue,
+	&mrGreen,
+	&mrRed,
+	&mrYellow
+};
 void loop()
 {
 	updateTimers();
 	
-	// update the walker position
-	mrBlue.Update(delta, PixelCount);
-	mrGreen.Update(delta, PixelCount);
-	mrRed.Update(delta, PixelCount);
-	mrYellow.Update(delta, PixelCount);
-	
-	// draw the walker
-	strip.SetPixelColor(mrBlue._position, mrBlue._color);
-	strip.SetPixelColor(mrGreen._position, mrGreen._color);
-	strip.SetPixelColor(mrRed._position, mrRed._color);
-	strip.SetPixelColor(mrYellow._position, mrYellow._color);
+	// update and draw animated objects
+	for (int i=0;i<4;++i)
+	{
+		animatedObjects[i]->Update(delta, PixelCount, strip);
+	}
 	
     strip.Show();
 }	
